26d97c54cc4d5853e95417bf708d7955e5a0fef6
Andrew Splin <andrewsplin@protonmail.com>
2020-04-18 15:45:41 -0700

    lots of stuff

---
+++ GUI/CoinCounter.gd
@@ -0,0 +1,17 @@
+extends Control
+
+onready var coin_amt = Globals.coins
+
+func _ready() -> void:
+	var coins = get_tree().get_nodes_in_group("coins")
+	if coins:
+		for coin in coins:
+			coin.connect("coin_grabbed", self, "_on_coin_grabbed")
+	update_coin_amt()
+
+func _on_coin_grabbed(value):
+	coin_amt += value
+	update_coin_amt()
+
+func update_coin_amt():
+	$HBoxContainer/Num.text = str(coin_amt)
--- actors/GeneralMonster.gd
+++ actors/GeneralMonster.gd
@@ -2,15 +2,32 @@ extends Area2D
 class_name GeneralMonster
 
 # Speed
-export var speed := Vector2(30, 0)
+export var speed := Vector2(50, 0)
 var monster_health = 100
 var animated_health = monster_health
 var monster_dead = false
 
 onready var bar = $HealthBar
 
+var monster_color = "blue"
+
 func _ready() -> void:
 	bar.max_value = monster_health
+	randomize()
+	var color_num = randi() % 3
+	match color_num:
+		0:
+			monster_color = "blue"
+		1:
+			monster_color = "green"
+		2:
+			monster_color = "orange"
+	$AnimatedSprite.play(monster_color + "_walk")
+
+	$ThumpSound.play()
+
+	$AnimatedSprite.rotation_degrees = -90
+	$CollisionShape2D.rotation_degrees = -90
 
 func _physics_process(delta: float) -> void:
 	 self.position += speed * delta
@@ -25,11 +42,20 @@ func game_over():
 
 func monster_died():
 	$CollisionShape2D.set_deferred("disabled", true)
+	set_physics_process(false)
+#	$AnimatedSprite.rotation_degrees = 0
+#	$CollisionShape2D.rotation_degrees = 0
+
+	randomize()
+	var num = randi() % 2
+	$AnimatedSprite.play(monster_color + "_death" + str(num))
 	var tween = $TransparencyTween
 	tween.interpolate_property(self, "modulate", Color(1,1,1,1), Color(1,1,1,0), 1)
+	$DeathSound.play()
 	tween.start()
 	yield(tween, "tween_completed")
 	queue_free()
+
 	Globals.monsters_died += 1
 	if Globals.monsters_died == Globals.max_monsters:
 		game_over()
@@ -56,3 +82,9 @@ func _on_GeneralMonster_area_entered(area: Area2D) -> void:
 		update_health()
 	if "Blockade" in area.name:
 		win_game()
+
+func _on_ThumpSound_finished() -> void:
+	$ThumpSound.stop()
+
+func _on_DeathSound_finished() -> void:
+	$DeathSound.stop()
