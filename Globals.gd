d700905f6a41e3242540491ed3488ac6f3bf560b
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-04-19 01:49:29 -0700

    yet more

--- Globals.gd
+++ Globals.gd
@@ -2,10 +2,20 @@ extends Node
 
 var monster_id = 1
 var monster_dict = {
-	1: "Big Lizard",
+	1: "Mega Lizard",
 	2: "Tanky Lizard",
-	3: "Bob the Lizard",
+	3: "Warrior Lizard",
 }
+
+var upgrades = {
+	"health": false,
+	"speed": false,
+	"fire": false,
+	"ice": false,
+}
+
+var finding_ice = false
+var finding_fire = false
 
 var max_monsters = 15
 var monsters_remaining = max_monsters
@@ -13,18 +23,38 @@ var monsters_died = 0
 var can_deploy = true
 
 var player_caught = false
-var coins = 0
+var coins = 500
 
-func _ready() -> void:
-	OS.window_maximized = true
+var monster_health = 100
+var monster_speed := Vector2(50, 0)
+var tank_monster_health = 160
+var tank_monster_speed := Vector2(30, 0)
+var warrior_monster_health = 50
+var warrior_monster_speed := Vector2(100, 0)
+
+var shop_monster = "mega"
+
+func try_connect_lanes():
 	var lanes = get_tree().get_nodes_in_group("lanes")
 	if lanes:
+		set_process(false)
 		for lane in lanes:
 			lane.connect("monster_deployed", self, "_on_monster_deployed")
 
+func _ready() -> void:
+	OS.window_maximized = true
+
+	if upgrades["health"]:
+		monster_health += 50
+	if upgrades["speed"]:
+		monster_speed.x += 50
+
+func _process(delta: float) -> void:
+	try_connect_lanes()
+
 func _on_monster_deployed():
 	can_deploy = false
-	yield(get_tree().create_timer(2, false), "timeout")
+	yield(get_tree().create_timer(2.0), "timeout")
 	can_deploy = true
 
 func get_monster_id():
