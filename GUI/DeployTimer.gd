c0adc6e0bed4f40bc0949c93fbae2452adcd0a8d
Andrew Splin <andrewsplin@protonmail.com>
2020-05-22 01:55:06 -0700

    minor changes

--- GUI/DeployTimer.gd
+++ GUI/DeployTimer.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 
 	$TextureProgress.hide()
 
+	# give them an id relating to each dino in the list
 	for i in range(0, DinoInfo.dino_list.size()):
 		if i in DinoInfo.timer_list:
 			continue
@@ -20,11 +21,11 @@ func _ready() -> void:
 		break
 
 func _on_dino_deployed():
-	if counting_down:
-		return
+	# only bother if the dino being deployed is our associated id
 	if DinoInfo.dino_id != id:
 		return
 
+	# start timer delay
 	var delay = DinoInfo.get_dino_timer_delay()
 	dino_timer.start(delay)
 
@@ -38,10 +39,12 @@ func update_progress_bar():
 
 	$TextureProgress.value = (dino_timer.time_left / delay) * 100
 
+	# once we're done, set bool to false and hide the circle
 	if dino_timer.time_left == 0:
 		counting_down = false
 		$TextureProgress.hide()
 
 func _process(delta: float) -> void:
+	# only update in _process if the delay is active
 	if counting_down:
 		update_progress_bar()
