27c6d8f7cd9cd22f611354162080dcfcdbeb62cf
Andrew Splin <andrewsplin@protonmail.com>
2020-05-23 15:34:12 -0700

    clean up dino selection-related code
    not amazing, will be finalized once the prospective ui from raj is detailed

--- DinoInfo.gd
+++ DinoInfo.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal dino_deployed
+
 var dino_id := 0
 var dino_list := [
 	preload("res://actors/dinos/MegaDino.tscn"),
@@ -10,11 +12,13 @@ var dinos_deploying := []
 var timer_list := []
 
 # {tanky: [health, dmg], mega: [health]}
-var upgrades_list := {}
+var upgrades_list := {"warrior": ["fire"], "tanky": ["ice"]}
 
 
 func has_upgrade(dino: String, upgrade: String) -> bool:
 	return dino in upgrades_list and upgrade in upgrades_list[dino]
+
+	print(upgrades_list)
 
 
 func add_upgrade(dino: String, upgrade: String) -> void:
@@ -36,8 +40,11 @@ func _on_dino_deployed():
 	add_child(dinos_deploying_timer)
 
 	dinos_deploying_timer.connect("timeout", self, "_on_dinos_deploying_timer_timeout", [dino_id])
-
 	dinos_deploying_timer.start(delay)
+
+	# re-emit signal
+	# allows for easier connection since it's a singleton
+	emit_signal("dino_deployed")
 
 
 func _on_dinos_deploying_timer_timeout(id):
@@ -46,12 +53,17 @@ func _on_dinos_deploying_timer_timeout(id):
 
 # find the delay based on the dino
 func get_dino_timer_delay():
+	return get_dino_property("deploy_delay")
+
+func get_dino_property(prop: String):
 	# instance it, get the variable we want, then remove it
 	var dino_scene = dino_list[dino_id]
 	var dino_instance = dino_scene.instance()
-	var delay = dino_instance.deploy_delay
+
+	var property = dino_instance.get(prop)
+
 	dino_instance.queue_free()
-	return delay
+	return property
 
 func reset_deploys():
 	dinos_deploying = []
