fc2d5e0e2740c0f7277f486f90a0e5a1b40ba114
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-04-25 12:08:34 -0700

    Remove all .gd files

--- Test.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	var data = load("res://src/actors/dinos/stats/MegaDino.tres")
-
-	print(data.hpStat.stats)
--- src/resources/Cost.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-
-export var stat_name = ""
-export(Array, int) var gold
-export(Array, int) var genes
