deb5260e191612a5da27e0bcdc9d30b6b58498ab
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-06-27 15:27:15 -0700

    Possible progress in collision shape fov

--- src/actors/BaseGuard.cs
+++ src/actors/BaseGuard.cs
@@ -6,9 +6,15 @@ public class BaseGuard : Area2D
 {
     AnimatedSprite animSprite;
 
+    Area2D FOVArea;
+    CollisionPolygon2D FOVCollision;
+
     public override void _Ready()
     {
         animSprite = (AnimatedSprite)FindNode("AnimatedSprite");
+
+        FOVArea = GetNode<Area2D>("FieldOfView");
+        FOVCollision = (CollisionPolygon2D)FindNode("FOVCollision");
 
         GD.Randomize();
         uint random = GD.Randi() % 3;
@@ -23,7 +29,31 @@ public class BaseGuard : Area2D
 
     void OnBaseGuardBodyEntered(Node2D body)
     {
-        Events.publishLevelFailed();
+        return;
+        if (body is KinematicBody2D)
+        {
+            Events.publishLevelFailed();
+            return;
+        }
+        // else, it's a tilemap; update the FOV cone
+        TileMap tileMap = (TileMap)body;
+
+        var tileCollisionPos = tileMap.MapToWorld(tileMap.WorldToMap(new Vector2(325, 0)));
+        GD.Print(tileMap.WorldToMap(FOVCollision.GlobalPosition));
+        var diff = FOVCollision.GlobalPosition - tileCollisionPos;
+        GD.Print(FOVCollision.GlobalPosition);
+        GD.Print(tileCollisionPos);
+        GD.Print(diff);
+
+        if (diff != null)
+        {
+            var newX = diff.x;
+            var newY = FOVCollision.Polygon[1].y;
+            var newVectorList = new Vector2[] { Vector2.Zero, new Vector2(newX, newY), new Vector2(newX, -newY) };
+            //  Polygon = newVectorList;
+            FOVCollision.SetDeferred("polygon", newVectorList);
+            Update();
+        }
     }
 
     void OnLevelFailed()
@@ -33,7 +63,6 @@ public class BaseGuard : Area2D
 
     public override void _Draw()
     {
-        var FOVCollision = (CollisionPolygon2D)FindNode("FOVCollision");
         DrawColoredPolygon(FOVCollision.Polygon, Colors.Red);
     }
 }
