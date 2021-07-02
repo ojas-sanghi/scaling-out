3f3d357c23eb68cd923f4fa1900a9a4f72c957cb
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-07-02 14:09:43 -0700

    Fix issue with special ability upgrades not saving

--- src/resources/SpecialStat.cs
+++ src/resources/SpecialStat.cs
@@ -6,9 +6,6 @@ public class SpecialStat : Stats
     [Export] public override string statName { get; set; } = "Special";
     [Export] public string special { get; set; } = "";
 
-    // "new" so that the order is different in the exported 
-    [Export] new int level { get; set; } = 0; //0 is locked, 1 is unlocked
-
     // hide the stats variable in the exported version
     new Array<double> stats;
 
